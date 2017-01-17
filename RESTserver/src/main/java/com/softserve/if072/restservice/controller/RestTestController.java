87ba35dde6e0b858d8f369092ebd5837c3105f5b
Mr.IPG <wadwad123@mail.ru>
2017-01-17 21:41:04 +0200

    Re-configured Spring using java-config instead of XML-config

--- RESTserver/src/main/java/com/softserve/if072/restservice/controller/RestTestController.java
+++ RESTserver/src/main/java/com/softserve/if072/restservice/controller/RestTestController.java
@@ -1,17 +1,24 @@
 package com.softserve.if072.restservice.controller;
 
 import com.softserve.if072.common.model.Unit;
+import com.softserve.if072.restservice.dao.UnitDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class RestTestController {
 
+    @Autowired
+    private UnitDAO unitDAO;
+
     @RequestMapping(value = "/")
     @ResponseBody
-    public Unit getTestUnit(){
-        Unit unit = new Unit(10, "meters");
-        return unit;
+    public List<Unit> getTestUnit(){
+        List<Unit> list = unitDAO.getAll();
+        return list;
     }
 }
