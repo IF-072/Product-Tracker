a3b24b22174cefafd7898951395009305cd4abfb
Vitaliy Malisevych <vitaliy_malisevych@ukr.net>
2017-01-19 23:12:48 +0200

    Add getAll() method in ProductController class

--- RESTserver/src/main/java/com/softserve/if072/restservice/controller/ProductController.java
+++ RESTserver/src/main/java/com/softserve/if072/restservice/controller/ProductController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping(value = "/product")
 public class ProductController {
@@ -14,9 +16,18 @@ public class ProductController {
     @Autowired
     private ProductServiceImpl productService;
 
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Product> getAll(){return productService.getAll();}
+
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
     @ResponseBody
     @ResponseStatus(value = HttpStatus.OK)
     public Product getById(@PathVariable int id){return productService.getById(id);}
 
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void insert(@RequestBody Product product){productService.insert(product);}
 }
