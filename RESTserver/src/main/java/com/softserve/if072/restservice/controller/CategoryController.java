a5515c5aa221456260857108bf55c1367ed5a676
Pavlo Bendus <svelaf71@gmail.com>
2017-01-20 13:51:34 +0200

    'CategoryController' updated (getAll(), delete() methods added).

--- RESTserver/src/main/java/com/softserve/if072/restservice/controller/CategoryController.java
+++ RESTserver/src/main/java/com/softserve/if072/restservice/controller/CategoryController.java
@@ -4,27 +4,46 @@ import com.softserve.if072.common.model.Category;
 import com.softserve.if072.restservice.service.impl.CategoryServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@RequestMapping(value = "/category")
 public class CategoryController {
 
     @Autowired
     private CategoryServiceImpl categoryService;
 
-    @RequestMapping(value = "/category/all")
+    @RequestMapping(value = "/all")
     @ResponseBody
     public List<Category> getAll() {
         return categoryService.getAll();
     }
 
-    @RequestMapping(value = "/category/{id}")
+    @RequestMapping(value = "/get/{id}")
     @ResponseBody
-    public Category getFirst(@PathVariable("id") int id) {
+    public Category getById(@PathVariable("id") int id) {
         return categoryService.getById(id);
+    }
+
+//    @RequestMapping(value = "/add", method = RequestMethod.POST)
+//    @ResponseBody
+//    public void insert(Category category) {
+//        System.out.println("LOL");
+//        Category category1 = new Category("Побутові товари", null);
+//        categoryService.insert(category1);
+//    }
+//
+//    @RequestMapping(value = "/update")
+//    @ResponseBody
+//    public void update(@RequestBody Category category) {
+//        categoryService.update(category);
+//    }
+
+    @RequestMapping(value = "/delete/{id}")
+    @ResponseBody
+    public void delete(@PathVariable("id") int id) {
+        categoryService.delete(id);
     }
 }
--- RESTserver/src/main/java/com/softserve/if072/restservice/dao/mybatisdao/CategoryDAO.java
+++ RESTserver/src/main/java/com/softserve/if072/restservice/dao/mybatisdao/CategoryDAO.java
@@ -5,14 +5,18 @@ import com.softserve.if072.common.model.User;
 import com.softserve.if072.restservice.dao.DAO;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+
 public interface CategoryDAO extends DAO<Category> {
 
-//    @Override
-//    @Results({
-//            @Result(property = "user", column = "user_id")
-//    })
-//    @Select("SELECT id, name, user_id FROM category")
-//    List<Category> getAll();
+    @Override
+    @Select("SELECT id, name, user_id FROM category")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "user", column = "user_id", javaType = User.class, one = @One(select = "com.softserve.if072.restservice.dao.mybatisdao.UserDAO.getAll"))
+    })
+    List<Category> getAll();
 
     @Override
     @Select("SELECT * FROM category WHERE id = #{id}")
@@ -23,15 +27,17 @@ public interface CategoryDAO extends DAO<Category> {
     })
      Category getByID(@Param("id") int id);
 
-//    @Override
-//    @Insert("INSERT INTO category(name, user_id) VALUES(#{name}, #{user.id})")
-//    void insert(Category category);
-//
-//    @Override
-//    @Update("UPDATE category SET name = #{name}, user_id = #{user.id} WHERE id = #{id}")
-//    void update(Category category);
-//
-//    @Override
-//    @Delete("DELETE FROM category WHERE id = #{id}")
-//    void delete(int id);
+    @Override
+    @Insert("INSERT INTO category(name, user_id) VALUES(#{name}, #{user.id})")
+    @Options(useGeneratedKeys = true)
+    void insert(Category category);
+
+
+    @Override
+    @Update("UPDATE category SET name = #{name}, user_id = #{user.id} WHERE id = #{id}")
+    void update(Category category);
+
+    @Override
+    @Delete("DELETE FROM category WHERE id = #{id}")
+    void delete(int id);
 }
