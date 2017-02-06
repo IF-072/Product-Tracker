3f51a04ce5070c2ec05200e215e9ae56a57a193a
Oleh Pochernin <pochernin.oleh@gmail.com>
2017-02-06 10:36:24 +0200

    Added UserProfileController and profile.jsp page

--- MVCapplication/src/main/java/com/softserve/if072/mvcapp/configuration/AppInitializer.java
+++ MVCapplication/src/main/java/com/softserve/if072/mvcapp/configuration/AppInitializer.java
@@ -2,6 +2,7 @@ package com.softserve.if072.mvcapp.configuration;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 import javax.servlet.Filter;
 
 public class AppConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
