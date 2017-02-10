923de8e597a0f44561fc2e7ef8ff000ad03bd817
Vitaliy Malisevych <vitaliy_malisevych@ukr.net>
2017-02-10 11:16:01 +0200

    Fixed product.jsp

--- MVCapplication/src/main/java/com/softserve/if072/mvcapp/controller/ProductPageController.java
+++ MVCapplication/src/main/java/com/softserve/if072/mvcapp/controller/ProductPageController.java
@@ -2,15 +2,21 @@ package com.softserve.if072.mvcapp.controller;
 
 import com.softserve.if072.common.model.Product;
 import com.softserve.if072.common.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * The class contains methods that handle the http requests from the product's page
@@ -20,14 +26,23 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/product")
+@PropertySource(value = {"classpath:application.properties"})
 public class ProductPageController {
+
+    @Value("${application.restProductURI}")
+    private String productUri;
+
     @RequestMapping("/")
     public String getProductPage(ModelMap model){
-        final String uri = "http://localhost:8080/rest/product/user/1";
+
+        int userId = 1;
+
+        final String uri = new String(productUri + "/user/{userId}");
+        Map<String, Integer> param = new HashMap<String, Integer>();
+        param.put("userId", userId);
 
         RestTemplate restTemplate = new RestTemplate();
-        Product[] result = restTemplate.getForObject(uri, Product[].class);
-        List products = Arrays.asList(result);
+        List<Product> products = restTemplate.getForObject(uri, List.class, param);
 
         model.addAttribute("products", products);
 
@@ -35,8 +50,10 @@ public class ProductPageController {
     }
 
     @PostMapping("/add")
-    public String addProduct(@RequestParam String name, @RequestParam String description){
-        final String uri = "http://localhost:8080/rest/product/";
+    @ResponseStatus(HttpStatus.OK)
+    public void addProduct(@RequestParam String name, @RequestParam String description){
+
+        final String uri = new String(productUri +"/");
 
         User user = new User();
         user.setId(1);
@@ -52,7 +69,5 @@ public class ProductPageController {
 
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.postForObject(uri, product1, Product.class);
-
-        return "product";
     }
 }
