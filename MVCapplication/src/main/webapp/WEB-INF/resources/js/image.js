f6bbb54795d87ea5f96eec4653bdfe12181e8e46
Vitaliy Malisevych <vitaliy_malisevych@ukr.net>
2017-03-10 09:27:28 +0200

    Add information dialogs

--- MVCapplication/src/main/webapp/WEB-INF/resources/js/image.js
+++ MVCapplication/src/main/webapp/WEB-INF/resources/js/image.js
@@ -2,16 +2,26 @@
  * Created by Vitaliy Malisevych on 01.03.2017.
  */
 
-var filesExt = ['jpg', 'jpeg', 'gif', 'png'];
+$(document).ready(function() {
+    $( "#dialogFileType" ).dialog({
+        autoOpen: false
+    });
 
-$('input[type=file]').change(function(){
-    var parts = $(this).val().split('.');
-    if(filesExt.join().search(parts[parts.length - 1]) != -1){
-        $( "input[type=submit]" ).prop( "disabled", false);
-    } else {
-        alert('Type of image file must be *.jpg, *.jpeg, *.gif, *.png !');
-        if($("submit[disabled=false]")){
-            $( "input[type=submit]" ).prop( "disabled", true);
+    $("#ok").click(function () {
+        $("#dialogFileType").dialog("close");
+    });
+
+    var filesExt = ['jpg', 'jpeg', 'gif', 'png'];
+
+    $('input[type=file]').change(function(){
+        var parts = $(this).val().split('.');
+        if(filesExt.join().search(parts[parts.length - 1]) != -1){
+            $( "input[type=submit]" ).prop( "disabled", false);
+        } else {
+            $("#dialogFileType").dialog("open");
+            if($("submit[disabled=false]")){
+                $( "input[type=submit]" ).prop( "disabled", true);
+            }
         }
-    }
-});
+    });
+})
---
+++ MVCapplication/src/main/webapp/WEB-INF/views/product/dialogFileType.jsp
@@ -0,0 +1,10 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+
+<div id="dialogFileType" hidden="hidden">
+    <h2 class="text-center"><spring:message code="image.fileType"/></h2>
+    <br />
+    <br />
+    <div class="center-block text-center">
+        <button id="ok" class="btn btn-success btn-reset-custom">Ok</button>
+    </div>
+</div>
